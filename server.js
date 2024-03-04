f9fd5861ade47ee94324f7ed466ecb0743e2216c
Katy Thompson <katimation@gmail.com>
2024-03-04 14:11:40 -0600

    added secret to env and turned posts and comments into partial templates

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const sess = {
   secret: process.env.SESS_SECRET,
   cookie: {},
   resave: false,
-  saveUnitialized: true,
+  saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
