07a6e07de3de18437495808787e317fbacd2364e
Katy Thompson <katimation@gmail.com>
2024-03-04 10:33:14 -0600

    added homepage that shows posts

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const routes = require('./controllers')
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 sequelize.sync({ force: false}).then(() => {
